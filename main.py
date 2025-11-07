3b8f5d5d6ec831414b2134e8f366c76b378ad95d
Twanano Ximhalani Rikhotso <twrikjhb025@student.wethinkcode.co.za>
2025-11-07 11:58:26 +0200

    update

--- main.py
+++ main.py
@@ -10,105 +10,124 @@ def draw_square(size:int, filled=False, char="*")-> str:
     Returns:
         str: A string representation of the square.
     """
-    pass
-
-    
-def draw_number_triangle(height:int)->str:
-    """
-    This function draws a triangle of numbers with the given height.
-    i.e height = 4
-    returns: 
-        1 
-        2 3 
-        4 5 6 
-        7 8 9 10
+    square = " "
+    for i in range(size):
+        if filled:
+            results += char * size
+        elif i == 0:
+            results += char * size
+        elif i == size - 1:          #Accessing the last char
+            results += + char * size
+        else:
+            results += char + " " * (size - 2) + char  
+        results += "\n"    #moving to the next line
         
-    Args:
-        height (int): height of the triangle.
+        return results
+    
+    print(draw_square(5))
+    print(draw_square(5, filled=True, char="#"))
 
-    Returns:
-        string : A string representation of the number triangle.
-    """
-    pass
-
-def factorial(n:int):
-    """
-    Calculate factorial of n (n!)
-    A factorial is the product of all positive integers less than or equal to n.
-    i.e factorial(5) or 5! = 5 * 4 * 3 * 2 * 1 = 120
-
-    n: non-negative integer
-    return: n!
-    """
-    pass
-    
-def bar_graph()->str:
-    """
-    This function draws a graph of averages per class:
-    
-    read data from grades.txt, there is data for 8 class and marks for 15 students per class
-    each row represents a class and each column represents a student marks
-    
-    Task is to draw a bar graph of averages per class:
-    each '*' represents 10 % 
-    i.e
-    
-    Class averages:
-    1: *****
-    2: ********
-    3: **
-    4: *****
-    ... etc.
-
-    returns:
-        str: string of the graph
-    """
-    pass
-
-
-def pascals_triangle(rows:int)->list[int]:
-    """ p(n, k) = n! / (k! * (n-k)!)
-    
-    
-    n: number of rows starting from 0
-    k: column number starting from 0
-    i.e 
-    rows 
-    0:              1
-    1:            1   1
-    2:          1   2   1
-    3:        1   3   3   1
-    4:      1   4   6   4   1
-    5:    1  5  10  10   5   1
-    6:  1  6  15  20  15   6   1
-    7:1  7 21  35  35  21   7   1
-    
-    example:
-        pascals_triangle(5)
-        returns [1, 5, 10, 10, 5, 1]
-        using 'p(n, k) = n! / (k! * (n-k)!)'
-    """
-    pass
-
-
-    
-    
-def main():
-    chars = ['#', '*', '+', '@', '%']
-    for i in range(3,8):
-        print(draw_square(i,False,char=chars[i-3]))
         
-    print()
-    print(draw_number_triangle(6))
-    print(factorial(5))
-    print()
-    print(pascals_triangle(5))
-    print(bar_graph())
-    print()
+    
+    # pass
+
+    
+# def draw_number_triangle(height:int)->str:
+#     """
+#     This function draws a triangle of numbers with the given height.
+#     i.e height = 4
+#     returns: 
+#         1 
+#         2 3 
+#         4 5 6 
+#         7 8 9 10
+        
+#     Args:
+#         height (int): height of the triangle.
+
+#     Returns:
+#         string : A string representation of the number triangle.
+#     """
+#     pass
+
+# def factorial(n:int):
+#     """
+#     Calculate factorial of n (n!)
+#     A factorial is the product of all positive integers less than or equal to n.
+#     i.e factorial(5) or 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+#     n: non-negative integer
+#     return: n!
+#     """
+#     pass
+    
+# def bar_graph()->str:
+#     """
+#     This function draws a graph of averages per class:
+    
+#     read data from grades.txt, there is data for 8 class and marks for 15 students per class
+#     each row represents a class and each column represents a student marks
+    
+#     Task is to draw a bar graph of averages per class:
+#     each '*' represents 10 % 
+#     i.e
+    
+#     Class averages:
+#     1: *****
+#     2: ********
+#     3: **
+#     4: *****
+#     ... etc.
+
+#     returns:
+#         str: string of the graph
+#     """
+#     pass
+
+
+# def pascals_triangle(rows:int)->list[int]:
+#     """ p(n, k) = n! / (k! * (n-k)!)
+    
+    
+#     n: number of rows starting from 0
+#     k: column number starting from 0
+#     i.e 
+#     rows 
+#     0:              1
+#     1:            1   1
+#     2:          1   2   1
+#     3:        1   3   3   1
+#     4:      1   4   6   4   1
+#     5:    1  5  10  10   5   1
+#     6:  1  6  15  20  15   6   1
+#     7:1  7 21  35  35  21   7   1
+    
+#     example:
+#         pascals_triangle(5)
+#         returns [1, 5, 10, 10, 5, 1]
+#         using 'p(n, k) = n! / (k! * (n-k)!)'
+#     """
+#     pass
+
+
+    
+    
+# def main():
+#     chars = ['#', '*', '+', '@', '%']
+#     for i in range(3,8):
+#         print(draw_square(i,False,char=chars[i-3]))
+        
+#     print()
+#     print(draw_number_triangle(6))
+#     print(factorial(5))
+#     print()
+#     print(pascals_triangle(5))
+#     print(bar_graph())
+#     print()
  
     
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
     
     
